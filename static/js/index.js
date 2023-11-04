498e3a7da3d2cdf960e5ce7c42085d13b7b6ede9
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-04 14:36:22 +0000

    added password visibility toggle

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,20 @@
-$(".left-section-desktop-navbar ul").css("background-color", "green")
+// allows the user to see their password
+const passwordViewToggle = $("#togglePassword");
+$(passwordViewToggle).click(function() {
+    // get the current type of the input box
+    const currentType = $("#password").attr("type");
+    
+    // if the current type is password {switch to text} else {leave as password};
+    $("#password").attr("type", currentType === "password" ? "text" : "password");
+    
+    // toggle the opacity class
+    $(this).toggleClass("active");
+
+    // toggle the icon classes
+    if ($(this).hasClass("bi-eye")) {
+        $(this).removeClass("bi-eye").addClass("bi-eye-slash");
+    } else {
+        $(this).removeClass("bi-eye-slash").addClass("bi-eye");
+    }
+    
+});
