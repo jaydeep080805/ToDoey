94116f9ebb13f32d13f4923016cb571aa87bc91c
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-04 15:29:56 +0000

    changed form styling so its bigger and uses better colouring

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField ,SubmitField, EmailField, PasswordField
+from wtforms import StringField, DateField ,SubmitField, EmailField, PasswordField, SelectField
 from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
     task = StringField("Task", validators=[DataRequired()])
     due_date = DateField("Due date")
+    category = SelectField("Catgegory", choices=["Personal", "Work"])
     submit = SubmitField("Submit")
 
 class SignUpForm(FlaskForm):
