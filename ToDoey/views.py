81997d7721566d8c2d4dcf6d2953e00dec407020
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-10 21:01:24 +0000

    added routes for changing name and email, also added more comments and changed some functions from utils to keep the code cleaner

--- ToDoey/views.py
+++ ToDoey/views.py
@@ -19,10 +19,16 @@ from flask_login import (
 from datetime import date, datetime
 
 # custom imports
-from .forms import TaskForm, SignUpForm, LoginForm
+from .forms import TaskForm, SignUpForm, LoginForm, ChangeNameForm, ChangeEmailForm
 from .models import UserInformation, TaskDataBase
 from . import db, csrf
-from .utils import hash_password, verify_password
+from .utils import (
+    hash_password,
+    verify_password,
+    validate_email,
+    get_user_by_email,
+    get_user_by_id,
+)
 
 
 main = Blueprint("main", __name__)
@@ -114,12 +120,6 @@ def about():
     return render_template("about.html")
 
 
-@main.route("/profile")
-def profile():
-    user_info = UserInformation.query.filter_by(email=current_user.email).first()
-    return render_template("profile.html", user_info=user_info)
-
-
 @main.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     form = SignUpForm()
@@ -127,17 +127,12 @@ def sign_up():
     if form.validate_on_submit():
         # grab the information from the form
         name_to_add = form.name.data
-        email_to_add = form.email.data
+        email_to_add = validate_email(form.email.data)
         password_to_add = hash_password(form.password.data)
-
-        # query the database to see if the email already exists in there
-        email_from_database = UserInformation.query.filter_by(
-            email=email_to_add
-        ).first()
 
         # if the email does exist then send a flash saying it exists
         try:
-            if email_from_database.email == email_to_add:
+            if not email_to_add:
                 flash("Email Already Exists!", "error")
                 return render_template("sign_up.html", form=form)
         except AttributeError:
@@ -199,6 +194,57 @@ def login():
             current_app.logger.error(f"Login error (Unknown): {e}")
 
     return render_template("login.html", form=form)
+
+
+@main.route("/profile")
+@login_required
+def profile():
+    user_info = get_user_by_email(current_user.email)
+    return render_template("profile.html", user_info=user_info)
+
+
+@main.route("/change_name", methods=["GET", "POST"])
+@login_required
+def change_name():
+    form = ChangeNameForm()
+
+    if form.validate_on_submit():
+        # gets the current users account by their id (my own function)
+        user_to_change = get_user_by_id(current_user.id)
+        # change their name
+        user_to_change.name = form.name.data
+        # then commit the changes and alert the user
+        db.session.commit()
+        flash("Successfully changed name", "success")
+        return redirect(url_for("main.profile"))
+
+    return render_template("change_name.html", form=form)
+
+
+@main.route("/change_email", methods=["GET", "POST"])
+@login_required
+def change_email():
+    form = ChangeEmailForm()
+
+    if form.validate_on_submit():
+        # if the current email entered is correct
+        # this will check if the emails are the same
+        # and you cannot use the same email more than once
+        if form.current_email.data == current_user.email:
+            new_email = form.new_email.data
+            # if the new email is not currently in the db (is valid)
+            if validate_email(new_email):
+                current_user.email = validate_email(new_email)
+
+                db.session.commit()
+                flash("Successfully changed email", "success")
+                return redirect(url_for("main.profile"))
+
+            # if the email is already in the db (not valid/used by someone else)
+            else:
+                flash("That email is already in use", "error")
+
+    return render_template("change_email.html", form=form)
 
 
 @main.route("/logout")
