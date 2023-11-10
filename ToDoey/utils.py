b8520d31823edae8d85d15d3a4274e114083d548
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-10 20:38:02 +0000

    added verify email function

--- ToDoey/utils.py
+++ ToDoey/utils.py
@@ -1,4 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from .models import UserInformation
 
 
 def hash_password(password):
@@ -7,3 +8,11 @@ def hash_password(password):
 
 def verify_password(hashed_password, password_from_form):
     return check_password_hash(hashed_password, password_from_form)
+
+
+def validate_email(form_email):
+    return (
+        False
+        if UserInformation.query.filter_by(email=form_email).first()
+        else form_email
+    )
