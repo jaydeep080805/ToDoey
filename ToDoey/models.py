bfff0784fb7669b0e822e190990b41f4fda3948b
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-11 18:15:49 +0000

    added comments

--- ToDoey/models.py
+++ ToDoey/models.py
@@ -7,8 +7,8 @@ from os import path
 basedir = path.abspath(path.dirname(__file__))
 
 
+# Database model for user tasks
 class TaskDataBase(db.Model):
-    # TODO add a description option so you can make a popout menu to edit the task
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey("user_information.id"), nullable=False)
     task = db.Column(db.String(), nullable=False)
@@ -18,6 +18,7 @@ class TaskDataBase(db.Model):
     user = db.relationship("UserInformation", back_populates="tasks")
 
 
+# Database model for user information
 class UserInformation(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
@@ -26,7 +27,7 @@ class UserInformation(db.Model, UserMixin):
     profile_pic = db.Column(db.String(), default=f"{basedir}static/images/default.png")
     creation_date = db.Column(db.Date, default=date.today())
     wants_notifications = db.Column(db.Boolean, default=True)
-    notification_time = db.Column(db.String, default='1 hour')
+    notification_time = db.Column(db.String, default="1 hour")
     tasks = db.relationship(
         "TaskDataBase", order_by=TaskDataBase.due_date, back_populates="user"
     )
