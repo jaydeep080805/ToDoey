b00c4ea583454e69b2dd4562d8d8edc764830be8
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-11 18:14:01 +0000

    added comments

--- ToDoey/forms.py
+++ ToDoey/forms.py
@@ -11,6 +11,7 @@ from wtforms import (
 from wtforms.validators import DataRequired
 
 
+# Form for creating a new task
 class TaskForm(FlaskForm):
     task = StringField("Task", validators=[DataRequired()])
     due_date = DateField("Due date")
@@ -18,6 +19,7 @@ class TaskForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+# Form for user sign-up
 class SignUpForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = EmailField("Email", validators=[DataRequired()])
@@ -25,23 +27,27 @@ class SignUpForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+# Form for user login
 class LoginForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
+# Form for changing user's name
 class ChangeNameForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
+# Form for changing user's email
 class ChangeEmailForm(FlaskForm):
     current_email = EmailField("Current Email", validators=[DataRequired()])
     new_email = EmailField("New Email", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
+# Form for changing user's password
 class ChangePasswordForm(FlaskForm):
     current_password = StringField("Current Password", validators=[DataRequired()])
     new_password = PasswordField("New Password", validators=[DataRequired()])
@@ -49,6 +55,7 @@ class ChangePasswordForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+# Form for changing user's profile picture
 class ChangeProfilePic(FlaskForm):
     profile_pic = FileField("Profile picture")
     submit = SubmitField("Submit")
