13b6b9c6b2f22f23e22c91e76d39c4eaab6bc73a
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-11 13:14:49 +0000

    changed way of sending email to use flask-mail and be asynchronous

--- ToDoey/utils.py
+++ ToDoey/utils.py
@@ -1,8 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import UserInformation
 from flask_mail import Message
-import smtplib
-from os import environ
+from . import mail
+from threading import Thread
+from flask import current_app
 
 
 # generates a hashed password
@@ -36,23 +37,33 @@ def get_user_by_id(id):
     return UserInformation.query.get(id)
 
 
+def send_async_email(app, msg):
+    # Flask-Mail needs to run within an application context,
+    with app.app_context():
+        try:
+            # Attempt to send the email.
+            mail.send(msg)
+        except Exception as e:
+            # Log any exceptions that occur during the mail sending process.
+            current_app.logger.error(f"An error occurred while sending an email: {e}")
+
+
 # function to send an email
 # takes the recipient and what information the user changed (name, email etc)
 def updated_info_message(recipient, change):
-    my_email = environ["EMAIL"]
-    password = environ["EMAIL_PASSWORD"]
-
-    # create a connection
-    connection = smtplib.SMTP("smtp.gmail.com", port=587)
-
-    # makes the connection secure with encryption
-    connection.starttls()
-    # login to our account
-    connection.login(user=my_email, password=password)
-
-    # send an email
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=recipient,
-        msg=f"Subject:{change} Change\n\nYour {change} has successfully been changed",
+    # Create a new message object with the subject, recipient, and body of the email.
+    msg = Message(
+        subject=f"{change} Change",
+        recipients=[recipient],
+        body=f"Your {change} has successfully been changed",
     )
+
+    # Retrieve the current Flask app instance.
+    # This is necessary because the new thread will not have access to the
+    # Flask app context by default.
+    app = current_app._get_current_object()
+
+    # Start a new thread to send the email.
+    # This allows the application to continue running and respond to web requests
+    # without waiting for the email to be sent.
+    Thread(target=send_async_email, args=(app, msg)).start()
