6b1d7bee0f8588661ff1d766ba5ab27782c91500
Jaydeep Rayat <jaydeep080805@gmail.com>
2023-11-11 13:20:15 +0000

    migrated database to add support for notifications

--- ToDoey/models.py
+++ ToDoey/models.py
@@ -25,6 +25,8 @@ class UserInformation(db.Model, UserMixin):
     password = db.Column(db.String(), nullable=False)
     profile_pic = db.Column(db.String(), default=f"{basedir}static/images/default.png")
     creation_date = db.Column(db.Date, default=date.today())
+    wants_notifications = db.Column(db.Boolean, default=True)
+    notification_time = db.Column(db.String, default='1 hour')
     tasks = db.relationship(
         "TaskDataBase", order_by=TaskDataBase.due_date, back_populates="user"
     )
